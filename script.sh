9da8041f9be3945e223720b89179144e49700b16
aydenbottos <aydenbottos12@gmail.com>
2021-10-18 12:00:58 +1100

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ echo "Last Modified on July 26, 2021"
 echo "Linux script"
 echo "The password used is CyberTaipan123!"
 ps -A | grep unattended-upgrade | awk '{print $1}' | xargs -r kill -9 $1
+ps -A | grep unattended-upgrade | awk '{print $1}' | xargs -r kill -9 $1
+ps -A | grep unattended-upgrade | awk '{print $1}' | xargs -r kill -9 $1
+ps -A | grep unattended-upgrade | awk '{print $1}' | xargs -r kill -9 $1
 dpkg --configure -a
 apt -y purge unattended-upgrades
 apt -y update
